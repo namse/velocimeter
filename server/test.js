02aec4ffca6cc03c6f515e7e785c787712cdf688
skatpgusskat <skatpgusskat@naver.com>
2017-09-07 17:12:09 +0900

    Add socket.io

--- server/test.js
+++ server/test.js
@@ -1,5 +1,12 @@
 const io = require('socket.io-client')
 
-const socket = io('http://localhost:3000');
+const socket = io('http://ec2-13-124-203-7.ap-northeast-2.compute.amazonaws.com:3000');
 
-socket.emit('velocity', 20);
+// socket.emit('velocity', 20);
+
+socket.on('connect', () => {
+  console.log('connect');
+});
+socket.on('disconnect', () => {
+  console.log('disconnect');
+});
